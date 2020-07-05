bf01728260b77b9fe56a681487bf2becf821fd1e
Johnfb32 <jfburton32@gmail.com>
2020-07-05 15:04:43 -0700

    implemented url for city

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function getCityNameInput (){
+    var cityName = document.getElementById("cityname").value
+}
+
+
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5408976eb6420d10cfd14a42e726bb54";
+  
+   
