d23a762e30583720ae85dca09a4e87f1419d6c0a
Johnfb32 <jfburton32@gmail.com>
2020-07-05 16:56:42 -0700

    restructured js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-function getCityNameInput (){
-    var cityName = document.getElementById("cityname").value
-}
+var getCityWeather = function() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=5408976eb6420d10cfd14a42e726bb54");
+};
+
+getCityWeather();
 
 
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5408976eb6420d10cfd14a42e726bb54";
-  
-   
+//"https://api.openweathermap.org/data/2.5/weather?q=London&appid=5408976eb6420d10cfd14a42e726bb54"
