1b1665e7dbe50ff5f49bd92447f448ebd0ee7946
Johnfb32 <jfburton32@gmail.com>
2020-07-05 17:39:31 -0700

    made url dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-var getCityWeather = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=5408976eb6420d10cfd14a42e726bb54");
-};
 
-getCityWeather();
+var getCityWeather = function (city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5408976eb6420d10cfd14a42e726bb54";
+    // format the github api url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    
+    });
+}
 
 
-//"https://api.openweathermap.org/data/2.5/weather?q=London&appid=5408976eb6420d10cfd14a42e726bb54"
+getCityWeather("London")
